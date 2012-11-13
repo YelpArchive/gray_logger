ce3fb6419d1b0dfe9d8764adadaecf68463ba746
Benjamin Behr <benny@digitalbehr.de>
2012-11-13 11:36:21 +0100

    remove the puts statements. doh...

--- lib/gray_logger/middleware.rb
+++ lib/gray_logger/middleware.rb
@@ -10,12 +10,10 @@ module GrayLogger
       configuration = symbolize_keys(options.delete(:configuration))
       self.gray_logger = if configuration && configuration[:host] && configuration[:port]
         size = ENV['GRAYLOGGER_SIZE'] || configuration.delete(:size) || "WAN"
-        puts configuration
         GELF::Logger.new(configuration.delete(:host), configuration.delete(:port), size, configuration)
       else
         options.delete(:logger)
       end
-      puts self.gray_logger
     end
 
     def call(env)
@@ -28,7 +26,6 @@ module GrayLogger
         raise e
       ensure
         message_store.status_code = status.to_i
-        puts message_store.to_message.to_hash.inspect
         gray_logger.notify!(message_store.to_message) unless gray_logger.nil?
       end
 
