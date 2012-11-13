f6f2445c03d713f7582485df1a5e7a15210165c7
Benjamin Behr <benny@digitalbehr.de>
2012-11-13 10:24:50 +0100

    calling #to_message on a message_store returns a message

--- test/gray_logger/message_store_test.rb
+++ test/gray_logger/message_store_test.rb
@@ -15,4 +15,10 @@ class MessageStoreTest < Test::Unit::TestCase
     assert_equal 'my custom message', @message_store.store[:my_custom_message], "the message_store haven't stored the added field: #{@message_store.store.inspect}"
   end
 
+  def test_to_message_creates_a_new_message_from_the_store
+    @message_store.my_custom_message = 'my custom message'
+    message = @message_store.to_message
+    assert message.is_a?(GrayLogger::Message), "this didn't return a GrayLogger::Message: #{message.class}"
+  end
+
 end
