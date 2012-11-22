a03de10d02623f870891a61c3b6b189938eb75ef
Benjamin Behr <benny@digitalbehr.de>
2012-11-22 13:28:45 +0100

    fix rails 3 issues with the railtie

--- lib/gray_logger/railtie.rb
+++ lib/gray_logger/railtie.rb
@@ -6,14 +6,14 @@ module GrayLogger
       initializer "gray_logger.configure_rails_initialization" do |app|
         configuration = YAML.load(File.read(Rails.root.join('config/gray_logger.yml')))[Rails.env]
         ::GrayLogger.configure(configuration)
+        ::GrayLogger.proxy.initialize_gray_logger!
+
+        ActionController::Base.send(:include, ::GrayLogger::HelperMethods)
+
         app.middleware.insert_after "ActionDispatch::ShowExceptions", "Rack::GrayLogger::Middleware"
       end
     rescue => e
       $stderr.puts("GrayLogger not configured. Please add config/gray_logger.yml")
-    end
-
-    initializer "gray_logger.include_logger" do |app|
-      ActionController::Base.send(:include, ::GrayLogger::HelperMethods)
     end
 
   end
